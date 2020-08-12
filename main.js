34308255880e82676f6b607ba7cb54eb0c9a5559
BarbaraMV <62711591+BarbaraMV@users.noreply.github.com>
2020-08-12 10:58:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    // carrusel del portafolio
+    $('.carousel').carousel({
+        dist:-60,
+        indicators: true
+      });
+  
+    $('.btnM').click(x => {
+      markMenu(x.target.id);
+    })
+  
+  });
+  
+  // cambia los estilos del menú
+  function markMenu(btn) {
+    $('.btnM').removeClass('active');
+    $('#' + btn).addClass('active');
+  }
